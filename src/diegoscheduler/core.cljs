3f92ecb0dc660e88aa51c6ee50fb5965dbd48c6a
Andrew Bruce <andrew@camelpunch.com>
2015-03-10 20:07:24 +0000

    Begin to flesh out the web form

--- src/diegoscheduler/core.cljs
+++ src/diegoscheduler/core.cljs
@@ -7,7 +7,11 @@
 
 (enable-console-print!)
 
-(def task-id (atom 1))
+(defonce task (atom {:id 1
+                     :domain "foo"
+                     :docker-image "docker://camelpunch/s3copier"
+                     :path "/usr/local/bundle/bin/bundle"
+                     :args "exec ./copy.rb mysource mydest"}))
 (def upch (chan))
 
 (go
@@ -22,21 +26,62 @@
         (go-loop []
           (when-let [message (<! ws-channel)]
             (js/console.log (str "Got this from server: " message))
-            (recur))))
-      )
-    ))
+            (recur)))))))
 
-(defn task-inc []
-  (swap! task-id inc)
-  (put! upch (str "Sending task " @task-id)))
+(defn inc-id [m]
+  (update-in m [:id] inc))
+
+(defn guid [t]
+  (str "task" (:id t)))
+
+(defn upload-task []
+  (put! upch @task)
+  (swap! task inc-id))
+
+(defn event-update [a attr]
+  (fn [e]
+    (swap! a #(assoc % attr (-> e .-target .-value)))))
+
+;; (add-task {:domain "foo"
+;;            :task_guid guid
+;;            :log_guid guid
+;;            :stack "lucid64"
+;;            :privileged false
+;;            :rootfs "docker:///camelpunch/simplesaver"
+;;            :action {:run {:path "/usr/bin/saver"
+;;                           :args ["/tmp/storage" "foobar"]}}
+;;            :completion_callback_url completion-callback-url
+;;            :result_file "/tmp/storage"
+;;            :disk_mb 1000
+;;            :memory_mb 1000})
+
+(defn input [a key label]
+  (let [id (str "task-" key)]
+    [:p
+     [:label {:for id} label]
+     [:input {:id id
+              :size 30
+              :value (key (deref a))
+              :on-change (event-update a key)}]]))
 
 (defn page []
   [:div
    [:h1 "Task Scheduler"]
    [:div
     [:h2 "Controls"]
-    [:button#add-task {:name (str "task" @task-id)
-                       :on-click task-inc} "Add Task " @task-id]]
+    [:p
+     [:label {:for "task_guid"} "GUID"]
+     [:input#task-guid {:name "task_guid"
+                        :disabled "disabled"
+                        :size 9
+                        :value (guid @task)}]]
+    (input task :domain "Domain")
+    (input task :docker-image "Docker image")
+    (input task :path "Path to executable")
+    (input task :args "Arguments (space sep)")
+    [:button#add-task {:name (str "task" (:id @task))
+                       :on-click upload-task} "Add " (guid @task)]]
+   [:p (str @task)]
    [:div
     [:h2 "Successful Tasks"]]
    [:div
