47a224b54a1bd98ff3f082d9a906759574e05707
Andrew Bruce <andrew@camelpunch.com>
2015-06-01 22:43:55 +0100

    Upgrade Clojure and Compojure
    Compojure brought in instaparse bug, hence the upgrade.

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
                  [org.danielsz/system "0.1.4"]
                  [environ "1.0.0"]
                  [http-kit "2.1.18"]
                  [clj-http "1.0.1"]
-                 [compojure "1.3.2"]
+                 [compojure "1.3.4"]
                  [jarohen/chord "0.6.0"]
                  [org.clojure/clojurescript "0.0-3123" :exclusions [org.clojure/tools.reader]]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
