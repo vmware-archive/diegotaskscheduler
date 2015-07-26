3358aafd667dc8f347b2eed96bfaaad5ea0e98e4
Andrew Bruce <andrew@camelpunch.com>
2015-07-26 21:39:46 +0100

    Fix task submitter test
    Synchronise on a promise to avoid races.

--- test/diegoscheduler/task_submitter_test.clj
+++ test/diegoscheduler/task_submitter_test.clj
@@ -7,13 +7,13 @@
 (deftest task-submission
   (testing "post fn gets called with /tasks path and task"
     (let [new-tasks (chan)
-          call (atom nil)
-          postfn (fn [url task] (reset! call [url task]))
+          call (promise)
+          postfn (fn [url task] (deliver call [url task]))
           task-submitter (new-task-submitter new-tasks
                                              postfn
                                              "http://my.api")
           running-task-submitter (start task-submitter)
           task {:task_guid "foo"}]
       (>!! new-tasks task)
-      (is (= ["http://my.api/tasks" task] @call))
+      (is (= ["http://my.api/tasks" task] (deref call 1000 "timed out waiting for call")))
       (stop running-task-submitter))))
